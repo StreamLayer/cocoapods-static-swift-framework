dc1859df97b642816a3d77502cf8527422bc3245
Vitaly Aminev <AVVS@users.noreply.github.com>
2019-05-27 22:25:36 -0700

    Update static_pod.rb

--- lib/cocoapods-static-swift-framework/patch/static_pod.rb
+++ lib/cocoapods-static-swift-framework/patch/static_pod.rb
@@ -1,7 +1,33 @@
 module Pod
-    class PodTarget
-        def static_framework?
-            return true
+    class Target        
+        # @return [Boolean] whether the target is built as a dynamic library
+        #
+        def build_as_dynamic_library?
+          return false
+        end
+
+        # @return [Boolean] whether the target is built as a framework
+        #
+        def build_as_framework?
+          return true
+        end
+
+        # @return [Boolean] whether the target is built as a library
+        #
+        def build_as_library?
+          return false
+        end
+
+        # @return [Boolean] whether the target is built statically
+        #
+        def build_as_static?
+          return true
+        end
+
+        # @return [Boolean] whether the target is built as a static framework
+        #
+        def build_as_static_framework?
+          return true
         end
     end
 end
